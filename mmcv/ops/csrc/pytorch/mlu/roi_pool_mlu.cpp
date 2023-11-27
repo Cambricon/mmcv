741b51f3d67f29ea57a257990e933c70a1a8163a
jiangyuhao <jiangyuhao@cambricon.com>
2023-11-27 16:52:56 +0800

    [TENSORFLOW-4474] feat(roi_pool): adapt roi_pool forward and backward
    (cherry picked from commit b7f9ae4db043226f8296d329da52bab8d155a339)

--- mmcv/ops/csrc/pytorch/mlu/roi_pool_mlu.cpp
+++ mmcv/ops/csrc/pytorch/mlu/roi_pool_mlu.cpp
@@ -9,38 +9,7 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *************************************************************************/
-#include "pytorch_device_registry.hpp"
-#include "pytorch_mlu_helper.hpp"
-
-void KernelRoiPoolForward(cnrtDim3_t k_dim, cnrtFunctionType_t k_type,
-                          cnrtQueue_t queue, cnrtDataType_t data_type,
-                          const void *input_data, const void *input_rois,
-                          const int batch, const int channels, const int height,
-                          const int width, const int pooled_height,
-                          const int pooled_width, const int rois_num,
-                          const float spatial_scale, void *output_data,
-                          int *argmax);
-
-void KernelRoiPoolBackward(cnrtDim3_t k_dim, cnrtFunctionType_t k_type,
-                           cnrtQueue_t queue, cnrtDataType_t k_dtype,
-                           const void *grad_output_ptr, const void *rois_ptr,
-                           const int *argmax_ptr, void *grad_input_ptr,
-                           const int box_num, const int pooled_height,
-                           const int pooled_width, const int channels,
-                           const int batch, const int height, const int width,
-                           const float spatial_scale);
-
-// policy function for forward
-static void policyFuncForward(const int bin_num, cnrtDim3_t *k_dim,
-                              cnrtFunctionType_t *k_type) {
-  auto core_num = torch_mlu::getDeviceAttr(cnrtAttrMcorePerCluster);
-  auto cluster_num = torch_mlu::getDeviceAttr(cnrtAttrClusterCount);
-  *k_type = CNRT_FUNC_TYPE_UNION1;
-  k_dim->x = core_num;
-  unsigned int use_cluster = bin_num / core_num + (bin_num % core_num > 0);
-  k_dim->y = use_cluster > cluster_num ? cluster_num : use_cluster;
-  k_dim->z = 1;
-}
+#include "mlu_common_helper.h"
 
 void ROIPoolForwardMLUKernelLauncher(Tensor input, Tensor rois, Tensor output,
                                      Tensor argmax, int pooled_height,
@@ -68,33 +37,20 @@ void ROIPoolForwardMLUKernelLauncher(Tensor input, Tensor rois, Tensor output,
   TORCH_CHECK(spatial_scale > 0 && spatial_scale <= 1,
               "spatial_scale should be within (0, 1], got ", spatial_scale);
 
-  // compute kernel params
-  auto batch = input.size(0);
-  auto height = input.size(2);
-  auto width = input.size(3);
-  auto channels = input.size(1);
-  auto rois_num = output.size(0);
-
-  if (output.numel() == 0) {
-    output = at::zeros({rois_num, channels, pooled_height, pooled_width},
-                       input.options());
-    return;
-  }
-  if (argmax.numel() == 0) {
-    argmax = at::zeros({rois_num, channels, pooled_height, pooled_width},
-                       argmax.options());
-    return;
-  }
-
   // zero element check
   if (input.numel() == 0 || rois.numel() == 0 || output.numel() == 0 ||
       argmax.numel() == 0) {
     return;
   }
 
+  auto channels = input.size(1);
+  auto rois_num = output.size(0);
+
   auto memory_format =
       torch_mlu::cnnl::ops::get_channels_last_memory_format(input.dim());
   auto input_ = torch_mlu::cnnl::ops::cnnl_contiguous(input, memory_format);
+  auto rois_contiguous =
+      torch_mlu::cnnl::ops::cnnl_contiguous(rois, rois.suggest_memory_format());
 
   at::Tensor output_ =
       at::empty({rois_num, channels, pooled_height, pooled_width},
@@ -103,45 +59,29 @@ void ROIPoolForwardMLUKernelLauncher(Tensor input, Tensor rois, Tensor output,
       at::empty({rois_num, channels, pooled_height, pooled_width},
                 argmax.options(), memory_format);
 
-  // calculate task dimension
-  cnrtDim3_t k_dim;
-  cnrtFunctionType_t k_type;
-  policyFuncForward(rois_num * pooled_height * pooled_width, &k_dim, &k_type);
-
-  // get compute queue
-  auto queue = torch_mlu::getCurQueue();
-
   // get ptr of tensors
   auto input_impl = torch_mlu::getMluTensorImpl(input_);
   auto input_ptr = input_impl->cnnlMalloc();
-  auto rois_impl = torch_mlu::getMluTensorImpl(rois);
+  auto rois_impl = torch_mlu::getMluTensorImpl(rois_contiguous);
   auto rois_ptr = rois_impl->cnnlMalloc();
   auto output_impl = torch_mlu::getMluTensorImpl(output_);
   auto output_ptr = output_impl->cnnlMalloc();
   auto argmax_impl = torch_mlu::getMluTensorImpl(argmax_);
   auto argmax_ptr = argmax_impl->cnnlMalloc();
 
-  // get comput dtype of input
-  cnrtDataType_t data_type = torch_mlu::toCnrtDtype(input_.dtype());
+  // set descriptor
+  MluOpTensorDescriptor input_desc, rois_desc, output_desc; 
+  input_desc.set_with_layout(input_, MLUOP_LAYOUT_NHWC);
+  rois_desc.set_with_layout(rois_contiguous, MLUOP_LAYOUT_ARRAY);
+  output_desc.set_with_layout(output_, MLUOP_LAYOUT_NHWC);
 
-  // launch kernel
-  CNLOG(INFO) << "Launch Kernel MLUKernelRoiPoolForward<<<" << k_dim.x << ", "
-              << k_dim.y << ", " << k_dim.z << ">>>";
+  auto handle = mluOpGetCurrentHandle();
 
-  KernelRoiPoolForward(k_dim, k_type, queue, data_type, input_ptr, rois_ptr,
-                       batch, channels, height, width, pooled_height,
-                       pooled_width, rois_num, spatial_scale, output_ptr,
-                       (int *)argmax_ptr);
+  TORCH_MLUOP_CHECK(mluOpRoiPoolingForward(
+      handle, MLUOP_POOLING_MAX, input_desc.desc(), input_ptr, rois_desc.desc(), rois_ptr,
+      spatial_scale, output_desc.desc(), output_ptr, (int *)argmax_ptr));
   output.copy_(output_);
   argmax.copy_(argmax_);
-}
-
-// policy function for backward
-static void policyFuncBackward(cnrtDim3_t *k_dim, cnrtFunctionType_t *k_type) {
-  *k_type = CNRT_FUNC_TYPE_UNION1;
-  k_dim->x = torch_mlu::getDeviceAttr(cnrtAttrMcorePerCluster);
-  k_dim->y = torch_mlu::getDeviceAttr(cnrtAttrClusterCount);
-  k_dim->z = 1;
 }
 
 void ROIPoolBackwardMLUKernelLauncher(Tensor grad_output, Tensor rois,
@@ -207,24 +147,18 @@ void ROIPoolBackwardMLUKernelLauncher(Tensor grad_output, Tensor rois,
   auto grad_output_ =
       torch_mlu::cnnl::ops::cnnl_contiguous(grad_output, memory_format);
   auto argmax_ = torch_mlu::cnnl::ops::cnnl_contiguous(argmax, memory_format);
-
-  int boxes_num = grad_output.size(0);
-  int no = grad_input.size(0);
-  int channels = grad_input.size(1);
-  int height = grad_input.size(2);
-  int width = grad_input.size(3);
-  auto grad_input_ = at::empty({no, channels, height, width},
-                               grad_input.options(), memory_format)
-                         .zero_();
+  auto rois_contiguous =
+      torch_mlu::cnnl::ops::cnnl_contiguous(rois, rois.suggest_memory_format());
+  memory_format =
+      torch_mlu::cnnl::ops::get_channels_last_memory_format(grad_input.dim());
+  auto grad_input_ =
+      torch_mlu::cnnl::ops::cnnl_contiguous(grad_input, memory_format);
 
   // get tensor impl
   auto grad_output_impl = torch_mlu::getMluTensorImpl(grad_output_);
-  auto rois_impl = torch_mlu::getMluTensorImpl(rois);
+  auto rois_impl = torch_mlu::getMluTensorImpl(rois_contiguous);
   auto argmax_impl = torch_mlu::getMluTensorImpl(argmax_);
   auto grad_input_impl = torch_mlu::getMluTensorImpl(grad_input_);
-
-  // get compute queue
-  auto queue = torch_mlu::getCurQueue();
 
   // get mlu ptr
   auto grad_output_ptr = grad_output_impl->cnnlMalloc();
@@ -232,19 +166,19 @@ void ROIPoolBackwardMLUKernelLauncher(Tensor grad_output, Tensor rois,
   auto argmax_ptr = argmax_impl->cnnlMalloc();
   auto grad_input_ptr = grad_input_impl->cnnlMalloc();
 
-  // calculate task dimension
-  cnrtDataType_t k_dtype = torch_mlu::toCnrtDtype(grad_input.dtype());
-  cnrtDim3_t k_dim;
-  cnrtFunctionType_t k_type;
-  policyFuncBackward(&k_dim, &k_type);
+  MluOpTensorDescriptor grad_output_desc, rois_desc, grad_input_desc, argmax_desc;
+  grad_output_desc.set_with_layout(grad_output_, MLUOP_LAYOUT_NHWC);
+  rois_desc.set_with_layout(rois_contiguous, MLUOP_LAYOUT_ARRAY);
+  grad_input_desc.set_with_layout(grad_input_, MLUOP_LAYOUT_NHWC);
+  argmax_desc.set_with_layout(argmax_, MLUOP_LAYOUT_NHWC);
 
-  CNLOG(INFO) << "Launch Kernel MLUKernelRoiPoolBackward<<<" << k_dim.x << ", "
-              << k_dim.y << ", " << k_dim.z << ">>>";
+  // get compute handle
+  auto handle = mluOpGetCurrentHandle();
 
-  KernelRoiPoolBackward(k_dim, k_type, queue, k_dtype, grad_output_ptr,
-                        rois_ptr, (int *)argmax_ptr, grad_input_ptr, boxes_num,
-                        pooled_height, pooled_width, channels, no, height,
-                        width, spatial_scale);
+  TORCH_MLUOP_CHECK(mluOpRoiPoolingBackward(
+      handle, MLUOP_POOLING_MAX, grad_output_desc.desc(), grad_output_ptr,
+      rois_desc.desc(), rois_ptr, argmax_desc.desc(), (int *)argmax_ptr,
+      spatial_scale, grad_input_desc.desc(), grad_input_ptr));
 
   grad_input.copy_(grad_input_);
 }
